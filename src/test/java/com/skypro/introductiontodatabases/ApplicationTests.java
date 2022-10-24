bed5e0199635f184a529ee9fd84c6aa51445f915
ANDRE <klai1@mail.ru>
2022-10-24 21:17:26 +0400

    Initial commit

---
+++ src/test/java/com/skypro/introductiontodatabases/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.introductiontodatabases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
